b866699c289f5cbe1c825e5de4159ff9e089d276
Thevans Vilella <thevans.vilella@gmail.com>
2021-05-18 13:56:30 -0300

    Update rds.conf.php
    changes in DB

--- rds.conf.php
+++ rds.conf.php
@@ -1,1 +1,1 @@
-<?php $RDS_URL='aaoh1jnjecjfuy.ccpu0pavm9z8.us-east-1.rds.amazonaws.com'; $RDS_DB='data'; $RDS_user='admin'; $RDS_pwd='Bazinga26'; $AFF_NUM='0';
+<?php $RDS_URL='aaoh1jnjecjfuy.ccpu0pavm9z8.us-east-1.rds.amazonaws.com'; $RDS_DB='data'; $RDS_user='admin'; $RDS_pwd='admin123'; $AFF_NUM='0';
